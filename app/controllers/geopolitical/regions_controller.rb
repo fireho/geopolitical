221ab73fe2402f9f438e650e39af44744eec3b3d
Marcos Piccinini <x@nofxx.com>
2014-12-06 19:04:12 -0200

    don't need this

--- app/controllers/geopolitical/regions_controller.rb
+++ app/controllers/geopolitical/regions_controller.rb
@@ -2,7 +2,7 @@ module Geopolitical
   # Main Regions Controller
   class RegionsController < GeopoliticalController
     inherit_resources
-    before_filter :get_relatives, only: [:new, :edit, :create, :update]
+    before_filter :set_nations, only: [:new, :edit, :create, :update]
 
     def collection
       @regions = Region.ordered.page(params[:page])
@@ -10,7 +10,7 @@ module Geopolitical
 
     private
 
-    def get_relatives
+    def set_nations
       @nations = Nation.all.map { |n| [n.name, n.id] }
     end
   end
