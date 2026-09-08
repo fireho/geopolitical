9f96f4ecdfa2f038459c616ac57d20a494c022d7
nofxx <chadart@gmail.com>
2026-09-08 01:07:37 -0300

    using mongoid

--- spec/dummy/config/environments/development.rb
+++ spec/dummy/config/environments/development.rb
@@ -31,15 +31,6 @@ Rails.application.configure do
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
-
-  # Highlight code that triggered database queries in logs.
-  config.active_record.verbose_query_logs = true
-
-  # Append comments with runtime information tags to SQL queries in logs.
-  config.active_record.query_log_tags_enabled = true
-
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
